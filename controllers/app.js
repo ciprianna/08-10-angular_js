7648079da8e2a1f7b7709c0a00d7dd5e97d7e0e3
Ciprianna <ciprianna17@gmail.com>
2015-08-10 13:49:14 -0500

    Added updateMessage function.

--- controllers/app.js
+++ controllers/app.js
@@ -2,4 +2,8 @@ angular.module('app', []);
 
 angular.module('app').controller('MainCtrl', function($scope){
   $scope.message = 'hello';
+
+  $scope.updateMessage = function(message) {
+    $scope.message = message;
+  }
 });
