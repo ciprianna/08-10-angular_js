5797f9a999edbedafb6786ad8edea5f453f7c1fb
Ciprianna <ciprianna17@gmail.com>
2015-08-10 15:47:17 -0500

    Model finished.

--- services/app.js
+++ services/app.js
@@ -3,5 +3,11 @@ angular.model('app', []);
 angular.module('app').factory('messages', function(){
   var messages = {};
 
+  messages.list = [];
+
+  messages.add = function(message){
+    messages.list.push({id: messages.list.length, text: message})
+  };
+
   return messages;
 });
