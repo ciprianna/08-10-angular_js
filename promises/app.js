7683bfa5481322ee9ac9e2225eecdc55b73d5218
Ciprianna <ciprianna17@gmail.com>
2015-08-11 12:22:52 -0500

    Changed the function to use $q to create the promise.

--- promises/app.js
+++ promises/app.js
@@ -1,17 +1,28 @@
 function getData($timeout, $q){
   return function(){
-    var defer = $q.defer()
 
-    // simulated asynch function
-    $timeout(function(){
-      if(Math.round(Math.random())){ // Setting a random number as the condition
-        defer.resolve('Data received!') // resolves if condition is met
-      } else {
-        defer.reject('Oh noes!') // else rejects the promise
-      }
-    }, 2000)
-
-    return defer.promise
+    // alternative method using $q to return the promise
+    return $q(function(resolve, reject){
+      $timeout(function(){
+        if(Math.round(Math.random())){
+          resolve('Data received!')
+        } else {
+          reject('Oh noes!')
+        }
+      }, 2000)
+    })
+    // var defer = $q.defer()
+    //
+    // // simulated asynch function
+    // $timeout(function(){
+    //   if(Math.round(Math.random())){ // Setting a random number as the condition
+    //     defer.resolve('Data received!') // resolves if condition is met
+    //   } else {
+    //     defer.reject('Oh noes!') // else rejects the promise
+    //   }
+    // }, 2000)
+    //
+    // return defer.promise
   }
 }
 
