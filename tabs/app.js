7c30b95d8fc3e57672711968f33f41323a18c28f
Ciprianna <ciprianna17@gmail.com>
2015-08-11 18:08:44 -0500

    Created tabset directive and started empty file for tabset.html.

--- tabs/app.js
+++ tabs/app.js
@@ -11,10 +11,28 @@ angular.module('app', [])
   return {
     restrict: "E",
     transclude: true,
-    template: "<h2>Hello, world!</h2> <div role='tabPanel' ng-transclude></div>",
+    template: "<h2>Hello, world!</h2> <div role='tabPanel' ng-transclude></div>", // to be inserted into document at directive location
     scope: { },
     link: function(scope, elem, attr){
-      
+
+    }
+  }
+})
+
+.directive('tabset', function(){
+  return {
+    restrict: "E",
+    transclude: true,
+    scope: { },
+    templateUrl: 'tabset.html', // Specifies an external template
+    bindToController: true, // Allows any values passed into directive's scope
+                            // via scope property are automatically accessible
+                            // in controller using 'this'
+    controllerAs: 'tabset', // Sets the name to reference so it's not reliant on
+                            // scope
+    controller: function(){ // Creates the Angular controller to use
+      var self = this
+      self.tabs = []
     }
   }
 })
