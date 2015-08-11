c271a221ca4a1a88f5878ad4126cdef965b73af0
Ciprianna <ciprianna17@gmail.com>
2015-08-11 17:58:10 -0500

    Added in function to display a sub-heading based on tab element   directive.

--- tabs/app.js
+++ tabs/app.js
@@ -5,3 +5,16 @@ angular.module('app', [])
 // Define directives here
 
 })(window);
+
+angular.module('app', [])
+.directive('tab', function(){
+  return {
+    restrict: "E",
+    transclude: true,
+    template: "<h2>Hello, world!</h2> <div role='tabPanel' ng-transclude></div>",
+    scope: { },
+    link: function(scope, elem, attr){
+      
+    }
+  }
+})
