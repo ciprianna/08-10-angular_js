96f15252c2a2c2dd2a447d58f60dea60fd29539e
Ciprianna <ciprianna17@gmail.com>
2015-08-11 14:03:13 -0500

    Changed the directive to be restricted to attributes.

--- directives/app.js
+++ directives/app.js
@@ -1,7 +1,15 @@
 angular.module('greetings', [])
 .directive("welcome", function(){ // First param is name, second is function
   return {
-    restrict: "E", // Restricts this directive to elements
-    template: "<div>Howdy there! You look splendid.</div>" // Specifies the html
+    restrict: "A", // Restricts to attributes
+    link: function(){
+      alert("Howdy!");
+    }
   }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // return {
+  //   restrict: "E", // Restricts this directive to elements
+  //   template: "<div>Howdy there! You look splendid.</div>" // Specifies the html
+  // }
 });
