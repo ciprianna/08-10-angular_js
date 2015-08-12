6d9ca76b8a8d5081481eace8bd665c27a00d27fe
Ciprianna <ciprianna17@gmail.com>
2015-08-12 17:51:44 -0500

    Added a log for the click event.

--- index_event_log/app.js
+++ index_event_log/app.js
@@ -1,1 +1,7 @@
 var app = angular.module("app", []);
+
+// Without using a controller, need to run the log in the rootScope
+// This will log the event
+app.run(function($rootScope, $log){
+  $rootScope.$log = $log;
+});
