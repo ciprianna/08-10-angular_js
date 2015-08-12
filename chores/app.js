91f186c907940a9610c2c67a16a33d9511d04486
Ciprianna <ciprianna17@gmail.com>
2015-08-12 12:19:24 -0500

    Added base files for chores.

---
+++ chores/app.js
@@ -0,0 +1,8 @@
+var app = angular.module("choreApp", []);
+
+app.directive("kid", function(){
+  return {
+    restrict: "E",
+    template: "<input type='text' ng-model='chore'>{{chore}}"
+  };
+});
