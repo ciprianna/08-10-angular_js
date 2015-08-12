3544b60edd419070bbd2eb1289ad3b8ede7b5f06
Ciprianna <ciprianna17@gmail.com>
2015-08-12 14:50:23 -0500

    Added an element to reference, rather than looking for a child.

--- element/app.js
+++ element/app.js
@@ -1,16 +1,34 @@
 var app = angular.module("app", []);
 
 app.directive("dumbPassword", function(){
+  // angular.element uses a jquery wrapper to create an element
+  var validElement = angular.element('<div>{{model.input}}</div>');
+
   return {
     restrict: "E",
     replace: true,
     templateUrl: "dumbpass.html",
-    link: function(scope, element){
-      scope.$watch("model.input", function(value){
-        if(value === "password"){
-          element.children(1).toggleClass("alert-box alert");
-        }
-      });
+    compile: function(tElem) { // Compiles an html string to a template function
+      tElem.append(validElement);
+
+      return function(scope) { // Compile returns a link function
+        scope.$watch("model.input", function (value) {
+          if(value === "password") {
+            validElement.toggleClass("alert-box alert");
+            // Can reference validElement, since it was created earlier
+            // Better reference than looking for a specific child.
+          }
+        });
+      }
     }
+    // link: function(scope, element){
+    //   // scope.$watch sets a listener on the model and waits for it to change;
+    //   // after it changes, new value is checked in the function
+    //   scope.$watch("model.input", function(value){
+    //     if(value === "password"){
+    //       element.children(1).toggleClass("alert-box alert");
+    //     }
+    //   });
+    // }
   };
 });
