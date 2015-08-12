abb1beea8e833db38b643fcc25c4d629b4ee4ef1
Ciprianna <ciprianna17@gmail.com>
2015-08-12 09:53:40 -0500

    Added a panel directive.

--- components/app.js
+++ components/app.js
@@ -9,3 +9,14 @@ app.directive("clock", function(){
     template: "<div>12:00pm {{ timezone }} </div>"
   };
 });
+
+app.directive("panel", function(){
+  return {
+    restrict: "E",
+    transclude: true,
+    scope: {
+      title: '@'
+    },
+    template: "<div style='border 3px solid #000000'>" + "<div class='alert-box'>{{title}}</div>" + "<div ng-transclude></div></div>"
+  };
+});
