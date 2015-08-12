1089ca5cfad4c104e7593b00c90bfad502e68ef9
Ciprianna <ciprianna17@gmail.com>
2015-08-12 14:53:15 -0500

    Moved the link return into it's own function to reference as the   return for the compile

--- element/app.js
+++ element/app.js
@@ -4,6 +4,16 @@ app.directive("dumbPassword", function(){
   // angular.element uses a jquery wrapper to create an element
   var validElement = angular.element('<div>{{model.input}}</div>');
 
+  var link = function(scope){
+    scope.$watch("model.input", function (value) {
+      if(value === "password") {
+        validElement.toggleClass("alert-box alert");
+        // Can reference validElement, since it was created earlier
+        // Better reference than looking for a specific child.
+      }
+    });
+  }
+
   return {
     restrict: "E",
     replace: true,
@@ -11,15 +21,7 @@ app.directive("dumbPassword", function(){
     compile: function(tElem) { // Compiles an html string to a template function
       tElem.append(validElement);
 
-      return function(scope) { // Compile returns a link function
-        scope.$watch("model.input", function (value) {
-          if(value === "password") {
-            validElement.toggleClass("alert-box alert");
-            // Can reference validElement, since it was created earlier
-            // Better reference than looking for a specific child.
-          }
-        });
-      }
+      return link;
     }
     // link: function(scope, element){
     //   // scope.$watch sets a listener on the model and waits for it to change;
