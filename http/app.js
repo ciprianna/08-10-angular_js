94fe0b5c98f432085debf6ffdce89ccd39062bfc
Ciprianna <ciprianna17@gmail.com>
2015-08-13 14:35:01 -0500

    Gets information from an API (not sure if it's supposed to be   posting somewhere, but no errors appear)

--- http/app.js
+++ http/app.js
@@ -1,12 +1,24 @@
 function testService($http) {
-  this.get = function() {}
+  // get function in the RESTful routes; returns API info from GET path
+  this.get = function() {
+    return $http.get('http://test-routes.herokuapp.com/test/hello')
+      .then(function(res){
+        // Return the data
+        return res.data.message;
+      })
+  }
 }
 
 
 function TestCtrl(testService) {
   var self = this;
 
-  self.getMessage = function() {}
+  self.getMessage = function() {
+    testService.get()
+      .then(function(res){
+        self.message = message;
+      })
+  }
 }
 
 angular.module('app', [])
