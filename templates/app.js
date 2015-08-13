375cba3da544f2488659230eb04bf487cafb67e1
Ciprianna <ciprianna17@gmail.com>
2015-08-13 09:56:56 -0500

    Converted template as a string to template as a remote to load.

--- templates/app.js
+++ templates/app.js
@@ -1,14 +1,18 @@
 (function(){
 
-  function TestCtrl() {
+  function TestCtrl($templateCache) {
     this.user = {name: 'Blake'};
+    console.log($templateCache.get('test.html')); // Prints the raw template to
+    // the console.
   }
 
   angular.module('app', ['ngRoute']) // Requires ngRoute, loaded on index
   .config(function($routeProvider){
     $routeProvider.when('/', {
       controller: 'TestCtrl as test',
-      template: 'Hello {{ test.user.name }}!'
+      templateUrl: 'test.html'
+      // template: 'Hello {{ test.user.name }}!' // Directly passes the String;
+      // not recommended to pass this way
     })
     .otherwise('/');
   })
