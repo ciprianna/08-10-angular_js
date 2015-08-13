bf401e3357fa8f043da76b0f3fd6480d7aff76f7
Ciprianna <ciprianna17@gmail.com>
2015-08-13 14:43:42 -0500

    Added post methods using http requests.

--- http/app.js
+++ http/app.js
@@ -1,24 +1,36 @@
 function testService($http) {
-  // get function in the RESTful routes; returns API info from GET path
-  this.get = function() {
-    return $http.get('http://test-routes.herokuapp.com/test/hello')
-      .then(function(res){
-        // Return the data
-        return res.data.message;
-      })
+  //http.post to POST data
+  this.upperCase = function(data) {
+    return $http.post('http://test-routes.herokuapp.com/test/uppercase', data)
   }
+
+
+  // // get function in the RESTful routes; returns API info from GET path
+  // this.get = function() {
+  //   return $http.get('http://test-routes.herokuapp.com/test/hello')
+  //     .then(function(res){
+  //       // Return the data
+  //       return res.data.message;
+  //     })
+  // }
 }
 
 
 function TestCtrl(testService) {
   var self = this;
 
-  self.getMessage = function() {
-    testService.get()
-      .then(function(res){
-        self.message = message;
+  self.postData = function(message){
+    testService.upperCase({message: message})
+      .success(function(body){
+        self.sendMessage = body.message;
       })
   }
+  // self.getMessage = function() {
+  //   testService.get()
+  //     .then(function(res){
+  //       self.message = message;
+  //     })
+  // }
 }
 
 angular.module('app', [])
