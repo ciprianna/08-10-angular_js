ecded0ccafbf5552083a5d5b13e82ac47f0e48ca
Ciprianna <ciprianna17@gmail.com>
2015-08-13 11:42:30 -0500

    Added multiple routes.

--- routes/app.js
+++ routes/app.js
@@ -1,5 +1,6 @@
 var app = angular.module("myApp", ['ngRoute']); // Dependent on ngRoute
 
+// Single page route configuration
 app.config(function($routeProvider){
   $routeProvider.when("/",
     {
@@ -7,10 +8,20 @@ app.config(function($routeProvider){
       controller: "AppCtrl",
       controllerAs: "app"
     }
-  );
+  )
+  .when('/cookies', // Passes a route for the API and the template it matches
+    {
+      template: "NOM NOM NOM NOM"
+    }
+  )
+  .otherwise({ // No route passed because it's a catchall for bad routes
+    template: "This route isn't set!"
+  });
 });
 
 app.controller("AppCtrl", function(){
   var self = this;
   self.message = "The app route is working!";
 });
+
+app.config
