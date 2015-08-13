49f9058c6cbc1ce4e68a00173d4a0d0edc87190b
Ciprianna <ciprianna17@gmail.com>
2015-08-13 11:16:43 -0500

    Added base files for the routes exercise.

---
+++ routes/app.js
@@ -0,0 +1,11 @@
+var app = angular.module("myApp", []);
+
+app.config(function($routeProvider){
+  $routeProvider.when("/",
+    {
+      templateUrl: "app.html",
+      controller: "AppCtrl",
+      controllerAs: "app"
+    }
+  );
+});
