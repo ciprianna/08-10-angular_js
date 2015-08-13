603cda23d181d627c7901f6e5d2ad4fe5d115c4e
Ciprianna <ciprianna17@gmail.com>
2015-08-13 11:27:48 -0500

    Uses a routeprovider to get the template.

--- routes/app.js
+++ routes/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['ngRoute']); // Dependent on ngRoute
 
 app.config(function($routeProvider){
   $routeProvider.when("/",
@@ -8,4 +8,9 @@ app.config(function($routeProvider){
       controllerAs: "app"
     }
   );
+});
+
+app.controller("AppCtrl", function(){
+  var self = this;
+  self.message = "The app route is working!";
 });
