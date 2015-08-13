e84b9612dfa30d7ae5cf7c5091057feb8ad481b8
Ciprianna <ciprianna17@gmail.com>
2015-08-13 09:22:13 -0500

    Transitions on buttons in a list.

--- animations/app.js
+++ animations/app.js
@@ -1,8 +1,16 @@
 function TestCtrl() {
   var self = this;
 
-  self.showBoxOne = false;
-  self.showBoxTwo = false;
+  // initialize an array
+  // buttons in html add/rm items in this array
+self.people = [
+  { name: "Eric Simons" },
+  { name: "Albert Pai" },
+  { name: "Matthew Greenster" }
+];
+
+  // self.showBoxOne = false;
+  // self.showBoxTwo = false;
 
 
 }
